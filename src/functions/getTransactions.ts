307f8b8677702678a711580fcc5c64e50a5fe05a
Lucas Cavalheiro <luccribeiro53@gmail.com>
2025-01-30 08:52:59 -0300

    feat: transaction functions

---
+++ src/functions/getTransactions.ts
@@ -0,0 +1,23 @@
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+import { supabase } from "../libs/supabase";
+
+export const handler = async (event: APIGatewayProxyEventV2) => {
+  const { data, error } = await supabase.from("transactions").select("*");
+
+  if(error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Error fetching transactions",
+        error
+      }),
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Get Transaction",
+      data
+    }),
+  };
+};
--- src/functions/updateTransaction.ts
+++ src/functions/updateTransaction.ts
@@ -1,10 +1,10 @@
 import type { APIGatewayProxyEventV2 } from "aws-lambda";
 
-export const createTransaction = async (event: APIGatewayProxyEventV2) => {
+export const handler = async (event: APIGatewayProxyEventV2) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Create new Transaction",
+      message: "Update Transaction",
       event: event
     }),
   };
