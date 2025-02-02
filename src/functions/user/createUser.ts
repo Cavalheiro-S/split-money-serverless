b0f809b77e8c02600db270c7fd0e8e0c214030cb
Lucas Cavalheiro <luccribeiro53@gmail.com>
2025-02-02 19:05:46 -0300

    feat: added validation lib

--- src/functions/user/createUser.ts
+++ src/functions/user/createUser.ts
@@ -1,28 +1,40 @@
-import { SignUpCommand } from "@aws-sdk/client-cognito-identity-provider";
+import { CodeDeliveryFailureException, InvalidPasswordException, SignUpCommand } from "@aws-sdk/client-cognito-identity-provider";
 import type { APIGatewayProxyEventV2 } from "aws-lambda";
 import { cognitoClient } from "../../libs/cognito";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  name: z.string().min(2)
+})
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
 
   try {
-
-    const body = JSON.parse(event.body || "{}");
-
-    console.log({body});
-    
+    const { success, data, error } = schema.safeParse(JSON.parse(event.body || "{}"));
+    if (!success) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Invalid request body",
+          error: error.issues
+        }),
+      };
+    }
 
     const command = new SignUpCommand({
       ClientId: process.env.COGNITO_CLIENT_ID,
-      Username: body.email,
-      Password: body.password,
+      Username: data.email,
+      Password: data.password,
       UserAttributes: [
         {
           Name: "email",
-          Value: body.email
+          Value: data.email
         },
         {
           Name: "name",
-          Value: body.name
+          Value: data.name
         }
       ]
     })
@@ -35,9 +47,28 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
       body: JSON.stringify({
         message: "Created new User",
         userId: UserSub
-      }),
+      }),       
     };
   } catch (error) {
+    if (error instanceof InvalidPasswordException) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Invalid Password",
+          error
+        }),
+      };
+    }
+
+    if (error instanceof CodeDeliveryFailureException) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Code Delivery Failure",
+          error
+        }),
+      };
+    }
     return {
       statusCode: 500,
       body: JSON.stringify({
