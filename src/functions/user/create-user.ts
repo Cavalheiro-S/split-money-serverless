2bf6185129510e9364bb787611872216ffafdf32
Lucas Cavalheiro <luccribeiro53@gmail.com>
2025-12-22 15:34:37 -0300

    feat(user): update user creation to use custom ID and fix Cognito user pool reference

--- src/functions/user/create-user.ts
+++ src/functions/user/create-user.ts
@@ -1,18 +1,16 @@
 import {
+  AdminDeleteUserCommand,
   CodeDeliveryFailureException,
   InvalidPasswordException,
-  SignUpCommand,
-  AdminDeleteUserCommand,
 } from '@aws-sdk/client-cognito-identity-provider';
 import type { APIGatewayProxyEventV2 } from 'aws-lambda';
-import { cognitoClient } from '../../libs/cognito';
 import { z } from 'zod';
+import { cognitoClient } from '../../libs/cognito';
 import { supabase } from '../../libs/supabase';
-import * as bcrypt from 'bcryptjs';
 
 const schema = z.object({
   email: z.string().email(),
-  password: z.string().min(8),
+  id: z.string(),
   name: z.string().min(2),
 });
 
@@ -31,32 +29,10 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
       };
     }
 
-    const command = new SignUpCommand({
-      ClientId: process.env.COGNITO_CLIENT_ID,
-      Username: data.email,
-      Password: data.password,
-      UserAttributes: [
-        {
-          Name: 'email',
-          Value: data.email,
-        },
-        {
-          Name: 'name',
-          Value: data.name,
-        },
-      ],
-    });
-
-    const { UserSub } = await cognitoClient.send(command);
-
-    // Hash the password with bcrypt before storing in database
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-
     const response = await supabase.from('users').insert({
-      id: UserSub,
+      id: data.id,
       email: data.email,
       name: data.name,
-      password: hashedPassword,
       login_method: 'cognito',
       updated_at: new Date(),
     });
@@ -64,7 +40,7 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
     if (response.error) {
       await cognitoClient.send(
         new AdminDeleteUserCommand({
-          UserPoolId: process.env.COGNITO_USER_POOL_ID,
+          UserPoolId: process.env.COGNITO_USERPOOL_ID,
           Username: data.email,
         })
       );
@@ -82,7 +58,7 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
       statusCode: 200,
       body: JSON.stringify({
         message: 'Created new User',
-        userId: UserSub,
+        userId: data.id,
       }),
     };
   } catch (error) {
